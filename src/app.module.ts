b1aa058f996d734fca32b8d059777f90984171bd
潘治颖 <panzhiying@cmii.chinamobile.com>
2025-01-09 10:56:28 +0800

    数据库表

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { Role } from './user/entities/role.entity';
+import { permissions } from './user/entities/permissions.entity';
+import { role_permissions } from './user/entities/role_permissions.entity';
+import { user_roles } from './user/entities/user_roles.entity';
 
 @Module({
   imports: [
@@ -15,7 +20,7 @@ import { UserModule } from './user/user.module';
       database: 'meeting_room_booking_system',
       synchronize: true,
       logging: true,
-      entities: [],
+      entities: [User, Role, permissions, role_permissions, user_roles],
       poolSize: 10,
       connectorPackage: 'mysql2',
       extra: {
